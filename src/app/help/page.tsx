93400722375554f29dfa68d3fba164f646711cb7
qinji <4584@qq.com>
2024-05-22 10:33:49 +0800

    预渲染

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -9,23 +9,25 @@ interface article {
 }
 
 async function Index() {
-	const revalidatedData = await fetch(
-		`${process.env.NEXT_PUBLIC_API_BASE_URL}/api`
-	);
+	// const revalidatedData = await fetch(
+	// 	`${process.env.NEXT_PUBLIC_API_BASE_URL}/api`
+	// );
 
-	const { data } = await revalidatedData.json();
+	// const { data } = await revalidatedData.json();
+	console.log(process.env);
 
 	return (
 		<section className={style.help}>
 			<div className="w1200">
+				{process.env.NEXT_PUBLIC_API_BASE_URL}
 				<div className={style.list}>
-					{data.map(({ id, article_title }: article, index: number) => {
+					{/* {data.map(({ id, article_title }: article, index: number) => {
 						return (
 							<div key={index} className={style.item}>
 								<Link href={`/help/${id}`}>{article_title}</Link>
 							</div>
 						);
-					})}
+					})} */}
 				</div>
 			</div>
 		</section>
